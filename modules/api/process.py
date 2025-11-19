e26bfef89784ad310a9a3373f40894e5b2839f68
awsr <43862868+awsr@users.noreply.github.com>
2025-11-19 01:26:50 -0800

    Revert "First pass at fixing `no-name-in-module` errors"
    This reverts commit 12720bb6d719c0ae7011b6991da2dd44c5ae6f73.

--- modules/api/process.py
+++ modules/api/process.py
@@ -113,7 +113,7 @@ class APIProcess():
         return ResMask(mask=image)
 
     def post_detect(self, req: ReqFace):
-        from modules.shared import yolo
+        from modules.shared import yolo # pylint: disable=no-name-in-module
         image = decode_base64_to_image(req.image)
         jobid = shared.state.begin('API-FACE', api=True)
         images = []
